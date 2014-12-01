0dd4dbeddf43d24c94131e538ede2e2397b7d939
Connor Newton <connor@phyrworks.co.uk>
2014-12-01 11:05:47 +0000

    Added LinescanView.

--- MATLAB/gui/views/LinescanView.m
+++ MATLAB/gui/views/LinescanView.m
@@ -1,9 +1,9 @@
-classdef MotorCurrentView < LineChartView
-    %MotorCurrentView
+classdef LinescanView < LineChartView
+    %LinescanView
     
     methods
-        % MotorCurrentView constructor
-        function obj = MotorCurrentView(attribute, varargin)
+        % LinescanView constructor
+        function obj = LinescanView(attribute, varargin)
             % parse input
             p = inputParser;
             addRequired (p, 'attribute', @ischar);
@@ -19,14 +19,18 @@ classdef MotorCurrentView < LineChartView
                 p.Results.attribute,...
                 'figure', p.Results.figure,...
                 'position', p.Results.position,... 
-                'title', ['Motor current: ', strrep(attribute, '_', '\_'],...
-                'xlabel', 'Time (s)',...
-                'ylabel', 'Current (A)'...
+                'title', ['Linescan Camera Image: ', strrep(attribute, '_', '\_')],...
+                'xlabel', 'Position (px)',...
+                'ylabel', 'Intensity (%/100)'...
             );
         end
         
         % draw chart 
-        % function obj = draw@LineChartView(obj, times, values)
+        function obj = draw(obj, values)
+            % set up data and plot
+            x = 1:128;
+            obj = draw@LineChartView(obj, x, values(1:128,1));
+        end
     end
     
 end
