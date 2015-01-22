f5b849aa58ba0fa7a622e7f7d9b80bd7d8a0ab5b
Connor Newton <connor@phyrworks.co.uk>
2015-01-22 14:25:23 +0000

    Added a speed filter generation utility. Shows estimated delay at various wheel speeds for a given numer of events per revolution.

---
+++ MATLAB/utils/buildSpeedFilter.m
@@ -0,0 +1,41 @@
+function [ coeffs ] = buildSpeedFilter( size, sigma, epr )
+%buildSpeedFilter
+
+    % generate coefficients
+    g = BuildGaussian(size, sigma);
+    
+    % find contribution intervals
+    sum = 0;
+    for i = 1:length(g)
+        sum = sum + g(i);
+        if (sum > 0.70)
+            cs = i;
+            break;
+        end
+    end
+    
+    disp('70% contribution delay:')
+    
+    rps = 100/60; %revs per second
+    eps = rps * epr; %events per second
+    dly = 1000*(cs/eps); %delay (ms)
+    disp([num2str(dly),'ms @100RPM']);
+    
+    rps = 300/60; %revs per second
+    eps = rps * epr; %events per second
+    dly = 1000*(cs/eps); %delay (ms)
+    disp([num2str(dly),'ms @300RPM']);
+    
+    rps = 600/60; %revs per second
+    eps = rps * epr; %events per second
+    dly = 1000*(cs/eps); %delay (ms)
+    disp([num2str(dly),'ms @600RPM']);
+    
+    rps = 1000/60; %revs per second
+    eps = rps * epr; %events per second
+    dly = 1000*(cs/eps); %delay (ms)
+    disp([num2str(dly),'ms @1000RPM']);
+    
+    coeffs = coeffs2csv(g);
+end
+
