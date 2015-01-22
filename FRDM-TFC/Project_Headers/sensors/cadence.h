4f35686091ad58a7c8ad40f06503019bb1eeebd9
Connor Newton <connor@phyrworks.co.uk>
2015-01-22 11:17:54 +0000

    Now buffering cadence sensor periods for use deriving a smoother speed measurement.

--- FRDM-TFC/Project_Headers/sensors/cadence.h
+++ FRDM-TFC/Project_Headers/sensors/cadence.h
@@ -13,13 +13,17 @@
 #include "config.h"
 
 typedef struct {
-  const TPM_MemMapPtr TPM;       //pointer to TPM module struct
-  const uint8_t       channel;   //channel occupied by sensor
-  uint16_t            cnv;       //timer value at last capture used for calculating next period
-  uint32_t            overflows; //number of times parent timer has overflowed during current period
-  uint32_t            period;    //most recent event period in ticks
-  const uint8_t       epr;       //events per revolution
-  uint8_t             flag;     //flag to signal new period available
+  const TPM_MemMapPtr TPM; //pointer to TPM module struct
+  const uint8_t channel;   //channel occupied by sensor
+  uint16_t cnv;            //timer value at last capture used for calculating next period
+  uint32_t overflows;      //number of times parent timer has overflowed during current period
+  uint32_t period;         //most recent event period in ticks
+  const uint8_t epr;       //events per revolution
+  uint8_t  flag;           //flag to signal new period available
+  struct {
+	  uint32_t data[CADENCE_BUFFER_SIZE];
+	  uint16_t pos;
+  } buffer;
 } CadenceSensor_s;
 
 extern CadenceSensor_s CadenceSensors[NUM_CADENCE_SENSORS];
