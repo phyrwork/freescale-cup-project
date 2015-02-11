8032819cdb2dfb302c3690faebdf952cc9a13d6d
Connor Newton <connor@phyrworks.co.uk>
2015-02-11 19:26:47 +0000

    Reverted MATLAB wheel speed attribute and view to revolutions per second, not per minute.

--- MATLAB/tftp/attributes/WheelSpeedAttribute.m
+++ MATLAB/tftp/attributes/WheelSpeedAttribute.m
@@ -18,15 +18,10 @@ classdef WheelSpeedAttribute < TftpAttribute
         function value = decode(obj, value)
             % cast byte stream
             value = decode@TftpAttribute(obj, value);
-            
-            % convert revolutions per second to per minute
-            value = value * 60;
         end
         
         % encode
         function value = encode(obj, value)
-            % convert revolutions per minute to per second
-            value = value / 60;
             
             % cast to byte stream
             value = encode@TftpAttribute(obj, value);
