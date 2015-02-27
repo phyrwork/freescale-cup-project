c223b77422a97a8673e67cf414d93c08e36a4c54
Connor Newton <connor@phyrworks.co.uk>
2015-02-27 15:53:05 +0000

    Added curve approximation tool. Varying Ki with torque PID error.

--- FRDM-TFC/Sources/control/motor/torque.c
+++ FRDM-TFC/Sources/control/motor/torque.c
@@ -12,8 +12,10 @@ static PID_s PID[NUM_MOTORS];
 MotorTorque_s MotorTorque[NUM_MOTORS];
 
 /* Control properties */
-#define TORQUE_KP 40.0f
-#define TORQUE_KI 40.0f
+#define TORQUE_KP 10.0f
+#define TORQUE_KI_LEN 	5
+#define TORQUE_KI_ERRS  {0, 0.001, 0.002, 0.0035, 0.007} 
+#define TORQUE_KI_GAINS {30, 60, 100, 250, 500}
 #define TORQUE_KD 0.0f
 
 #define TORQUE_PID_GAIN_C 1.0f
@@ -36,7 +38,7 @@ void InitMotorTorqueControl()
 		/* Initialise PID controller */
 		torque->PID = &PID[i];
 		torque->PID->Kp = TORQUE_KP;
-		torque->PID->Ki = TORQUE_KI;
+		//torque->PID->Ki = TORQUE_KI;
 		torque->PID->Kd = TORQUE_KD;
 		torque->PID->time = 0;
 		torque->PID->value = 0;
@@ -46,6 +48,26 @@ void InitMotorTorqueControl()
 		torque->PID->value_max = MAX_DUTY;
 		torque->PID->value_min = -MAX_DUTY;
 	}
+}
+
+float _curveApprox(uint32_t const p, float * const x, float * const y, float const in)
+{
+	if (in <= x[0]) return y[0];
+	
+	for (uint32_t j = 1; j < p; ++j)
+	{
+		if(in <= x[j] && in >= x[j-1]) 
+		{
+			float c = y[j-1];
+			float dy = y[j] - y[j-1];
+			float dx = x[j] - x[j-1];
+			float m = dy/dx;
+			
+			return c + ( (in - x[j-1]) * m);
+		}
+	}
+	
+	return y[p-1];
 }
 
 void UpdateMotorTorque(MotorTorque_s *torque)
@@ -66,6 +88,12 @@ void SetMotorTorque(MotorTorque_s *torque, float command)
 	float tspd = torque->speed->value > 31 ? 31 : torque->speed->value;
 	float tsat = (TORQUE_SPEED_M * tspd) + TORQUE_SPEED_C;
 	//command = command > tsat ? tsat : (command < -tsat ? -tsat : command);
+	
+	//sliding controller gain
+	float const x[] = TORQUE_KI_ERRS;
+	float const y[] = TORQUE_KI_GAINS;
+	
+	torque->PID->Ki = _curveApprox(TORQUE_KI_LEN, x, y, fabsf(torque->PID->error));
 	
 	UpdatePID(torque->PID, command, torque->value);
 	SetMotorPWM(torque->pwm, torque->PID->value);
