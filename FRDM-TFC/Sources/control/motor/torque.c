0e775112eacf64926c964ad8dcf0884a56c6a517
Connor Newton <connor@phyrworks.co.uk>
2015-03-02 15:52:08 +0000

    Updated torque PID constants. Performance is acceptable - some overshoot/instability at large errors.

--- FRDM-TFC/Sources/control/motor/torque.c
+++ FRDM-TFC/Sources/control/motor/torque.c
@@ -6,20 +6,23 @@
  */
 
 #include "control/motor/torque.h"
+#include <math.h>
 
 /* Define torque control structs */
 static PID_s PID[NUM_MOTORS];
 MotorTorque_s MotorTorque[NUM_MOTORS];
 
 /* Control properties */
-#define TORQUE_KP 10.0f
-#define TORQUE_KI_LEN 	5
-#define TORQUE_KI_ERRS  {0, 0.001, 0.002, 0.0035, 0.007} 
-#define TORQUE_KI_GAINS {30, 60, 100, 250, 500}
+#define TORQUE_KP 30.0f
+#define TORQUE_KI 800.0f
 #define TORQUE_KD 0.0f
 
-#define TORQUE_PID_GAIN_C 1.0f
-#define TORQUE_PID_GAIN_M 0.1f
+#define TORQUE_PID_SPEED_C 1.0f
+#define TORQUE_PID_SPEED_M 0.0f
+#define TORQUE_PID_ERR_C 1.0f
+#define TORQUE_PID_ERR_M 0.0f
+
+                 
 
 void InitMotorTorqueControl()
 {
@@ -38,16 +41,15 @@ void InitMotorTorqueControl()
 		/* Initialise PID controller */
 		torque->PID = &PID[i];
 		torque->PID->Kp = TORQUE_KP;
-		//torque->PID->Ki = TORQUE_KI;
+		torque->PID->Ki = TORQUE_KI;
 		torque->PID->Kd = TORQUE_KD;
 		torque->PID->time = 0;
 		torque->PID->value = 0;
-		torque->PID->value_max = 1;
-		torque->PID->value_min = -1;
-		torque->PID->antiwindup = ANTI_WINDUP_CLAMP;
 		torque->PID->value_max = MAX_DUTY;
 		torque->PID->value_min = -MAX_DUTY;
+		torque->PID->antiwindup = ANTI_WINDUP_CLAMP;
 	}
+	
 }
 
 float _curveApprox(uint32_t const p, float * const x, float * const y, float const in)
@@ -85,16 +87,21 @@ void SetMotorTorque(MotorTorque_s *torque, float command)
 	command = command > TORQUE_MAX ? TORQUE_MAX : (command < -TORQUE_MAX ? -TORQUE_MAX : command);
 	
 	//recalculate saturation
-	float tspd = torque->speed->value > 31 ? 31 : torque->speed->value;
-	float tsat = (TORQUE_SPEED_M * tspd) + TORQUE_SPEED_C;
+	//float tspd = torque->speed->value > 31 ? 31 : torque->speed->value;
+	//float tsat = (TORQUE_SPEED_M * tspd) + TORQUE_SPEED_C;
 	//command = command > tsat ? tsat : (command < -tsat ? -tsat : command);
 	
 	//sliding controller gain
-	float const x[] = TORQUE_KI_ERRS;
-	float const y[] = TORQUE_KI_GAINS;
+	float speedm = TORQUE_PID_SPEED_C + (torque->speed->value * TORQUE_PID_SPEED_M);
+	float errorm = TORQUE_PID_ERR_C + (fabsf(torque->PID->error) * TORQUE_PID_ERR_M);
+	torque->PID->Kc = speedm * errorm;
 	
-	torque->PID->Ki = _curveApprox(TORQUE_KI_LEN, x, y, fabsf(torque->PID->error));
+	//float const x[] = TORQUE_KI_ERRS;
+	//float const y[] = TORQUE_KI_GAINS;
+	//torque->PID->Ki = _curveApprox(TORQUE_KI_LEN, x, y, fabsf(torque->PID->error));
 	
 	UpdatePID(torque->PID, command, torque->value);
+	
+	
 	SetMotorPWM(torque->pwm, torque->PID->value);
 }
