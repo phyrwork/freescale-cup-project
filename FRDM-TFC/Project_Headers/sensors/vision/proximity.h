a7d3d83b73939c64a1ae8ffc6af5306e75dafed9
Connor Newton <connor@phyrworks.co.uk>
2015-03-11 16:13:21 +0000

    Added proximity sensor source to project.

---
+++ FRDM-TFC/Project_Headers/sensors/vision/proximity.h
@@ -0,0 +1,7 @@
+#ifndef CORNER_PROXIMITY_SENSOR
+#define CORNER_PROXIMITY_SENSOR
+
+void  InitProximitySensor();
+float getProximity();
+
+#endif
---
+++ FRDM-TFC/Sources/sensors/vision/proximity.c
@@ -0,0 +1,41 @@
+#include "sensors/vision/proximity.h"
+#include "sensors/vision/positioning.h" //import edge detection
+#include "sensors/vision/linescan.h"    //import linescan1
+#include "config.h"
+
+#include <math.h>
+
+#define ANGF 54.9f
+#define ANGV 57.6f
+//#define VH   0.225f
+#define VN   0.225f
+#define P    128
+
+#define SIZEOF_EDGEBUFFER 8
+#define PROX_DY_T 100
+#define PROX_RY_T 300
+
+float dtable[P];
+
+void InitProximitySensor()
+{
+	//construct proximity table from given parameters
+	for (int i = 0; i < P; ++i)
+	{
+		dtable[i] = tanf( ANGF - (ANGV/2) + (i+1)*( ANGV/( (2*P)+1) ) ) - VN;
+	}
+}
+
+float getProximity()
+{
+	int16_t dy[128];
+
+	diff(linescan[0].image, dy, 128);
+	filtfilt4ma(dy, dy);
+
+	Edge_s edges[SIZEOF_EDGEBUFFER];
+	uint8_t features = findEdges(&edges, dy, PROX_DY_T, PROX_RY_T);
+
+	for (int i = 0; i < features; ++i)
+	if (edges[i].type == EDGE_TYPE_RISING) return dtable[edges[i].pos];
+}
